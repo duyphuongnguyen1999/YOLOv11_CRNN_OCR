b3077f7609844ed06fc6bf5f8e5f934e40117f80
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2025-01-09 21:31:19 +0700

    Encapsulating all configuration constants into Config class

--- config.py
+++ config.py
@@ -1,4 +1,19 @@
-SEED = 0
-VAL_SIZE = 0.2
-TEST_SIZE = 0.125
-IS_SHUFFLE = True
+import os
+
+
+class Config:
+    def __init__(
+        self,
+        dataset_root_dir: str = "datasets/SceneTrailTrain",
+        sava_processed_data_dir: str = "datasets/yolo_data",
+        random_seed: int = 0,
+        val_size=0.2,
+        test_size=0.125,
+        is_shuffle=True,
+    ):
+        self.dataset_root_dir = os.path.normpath(dataset_root_dir)
+        self.save_processed_data_dir = os.path.normpath(sava_processed_data_dir)
+        self.random_seed = random_seed
+        self.val_size = val_size
+        self.test_size = (test_size,)
+        self.is_shuffle = is_shuffle
