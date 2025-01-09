92f4a2b1e44b0b78d3eb8b8d0f17d25b08d653a9
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2025-01-09 21:49:16 +0700

    Add type hinting for __init__() arguments

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 
 
 class Config:
@@ -6,14 +7,14 @@ class Config:
         self,
         dataset_root_dir: str = "datasets/SceneTrailTrain",
         sava_processed_data_dir: str = "datasets/yolo_data",
-        random_seed: int = 0,
-        val_size=0.2,
-        test_size=0.125,
-        is_shuffle=True,
+        random_seed: Optional[int] = 0,
+        val_size: Optional[float] = 0.2,
+        test_size: Optional[float] = 0.125,
+        is_shuffle: Optional[bool] = True,
     ):
         self.dataset_root_dir = os.path.normpath(dataset_root_dir)
         self.save_processed_data_dir = os.path.normpath(sava_processed_data_dir)
         self.random_seed = random_seed
         self.val_size = val_size
-        self.test_size = (test_size,)
+        self.test_size = test_size
         self.is_shuffle = is_shuffle
