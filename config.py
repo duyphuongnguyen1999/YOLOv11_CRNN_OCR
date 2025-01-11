c91fb2dc1b13f130a3ecf061660925b3557613ca
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2025-01-11 20:49:39 +0700

    feat: Add save_ocr_data_dir attribute to <class> DataProcessorConfig

--- config.py
+++ config.py
@@ -7,14 +7,16 @@ class DataProcessorConfig:
     def __init__(
         self,
         dataset_root_dir: str = "datasets/SceneTrialTrain",
-        sava_processed_data_dir: str = "datasets/yolo_data",
+        sava_yolo_data_dir: str = "datasets/yolo_data",
+        save_ocr_data_dir: str = "datasets/ocr_data",
         random_seed: Optional[int] = 0,
         val_size: Optional[float] = 0.2,
         test_size: Optional[float] = 0.125,
         is_shuffle: Optional[bool] = True,
     ):
         self.dataset_root_dir = os.path.normpath(dataset_root_dir)
-        self.save_processed_data_dir = os.path.normpath(sava_processed_data_dir)
+        self.save_yolo_data_dir = os.path.normpath(sava_yolo_data_dir)
+        self.save_ocr_data_dir = os.path.normpath(save_ocr_data_dir)
         self.random_seed = random_seed
         self.val_size = val_size
         self.test_size = test_size
